48350771851b64e2866820078ab74e5cf245ce32
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-11-21 19:07:53 -0400

    auto add .bash_profile to .bashrc

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-    echo "Installing YADR for the first time"
-    git clone https://github.com/gullitmiranda/dotfiles "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles"
-    [ "$1" == "ask" ] && export ASK="true"
-    rake install
+  echo "Installing YADR for the first time"
+  git clone https://github.com/gullitmiranda/dotfiles "$HOME/.dotfiles"
+  cd "$HOME/.dotfiles"
+  [ "$1" == "ask" ] && export ASK="true"
+  echo "
+if [ -f ~/.dotfiles/.bash_profile ]; then
+. ~/.dotfiles/.bash_profile
+fi " >> ~/.bashrc
+  rake install
 else
-    echo "You DotFiles is already installed"
+  echo "You DotFiles is already installed"
 fi
